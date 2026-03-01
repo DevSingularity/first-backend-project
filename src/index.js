c6dd634236328939b49fec4f37a02610084b152a
Saman Pandey <singularity.1@hotmail.com>
2026-03-01 16:34:02 +0530

    refactor: improve error handling in Db connection promise

--- src/index.js
+++ src/index.js
@@ -5,4 +5,17 @@ dotenv.config({
     path: '../env'
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGODB CONNECTION ERROR: ", err);
+    });
